0955198f24fc9bbfdcdafe76174f6482bc99792a
janluyleonardo <janluy.moreno@gmail.com>
2025-08-04 19:17:30 -0500

    ajustes de favicon en las pantall;as de app y guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,50 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+  <title>{{ config('app.name', 'test-riesgos') }}</title>
 
-        <!-- Styles -->
-        @livewireStyles
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-        <link rel="stylesheet" href="{{ asset('css/app.css?V='.now()->format('H.s')) }}">
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.css?V='.now()->format('H.s')) }}">
-        <link rel="stylesheet" href="{{ asset('css/formulario.css?v='.now()->format('H.s')) }}">
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+  {{-- favicon --}}
+  <link rel="icon" type="image/x-icon" href="{{ env('FAVICON_PATH', asset('images/logo.png')) }}">
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+  <!-- Styles -->
+  @livewireStyles
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+  <!-- Scripts -->
+  {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+  <link rel="stylesheet" href="{{ asset('css/app.css?V=' . now()->format('H.s')) }}">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.css?V=' . now()->format('H.s')) }}">
+  <link rel="stylesheet" href="{{ asset('css/formulario.css?v=' . now()->format('H.s')) }}">
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+<body class="font-sans antialiased">
+  <x-jet-banner />
+
+  <div class="min-h-screen bg-gray-100">
+    @livewire('navigation-menu')
+
+    <!-- Page Heading -->
+    @if (isset($header))
+      <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          {{ $header }}
         </div>
+      </header>
+    @endif
 
-        @stack('modals')
+    <!-- Page Content -->
+    <main>
+      {{ $slot }}
+    </main>
+  </div>
 
-        @livewireScripts
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
-    </body>
+  @stack('modals')
+
+  @livewireScripts
+  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+</body>
+
 </html>
